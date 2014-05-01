464ce6b418c0cdaa4167d6b81daacd49ba78c118
Alexandru Avram <alexanderavram@gmail.com>
2014-05-02 02:19:51 +0300

    interogate each device
    still needs to be added to db

--- devices.py
+++ devices.py
@@ -5,7 +5,9 @@ import MySQLdb as mdb
 import pycurl
 import sys
 import json
+from StringIO import StringIO 
 
+devices = 0
 
 def create_devices_table():
 
@@ -14,9 +16,15 @@ def create_devices_table():
 
         with con:
             cur = con.cursor()
+            cur.execute("DROP TABLE IF EXISTS Readings")
             cur.execute("DROP TABLE IF EXISTS Devices")
             cur.execute("CREATE TABLE Devices(Id INT PRIMARY KEY AUTO_INCREMENT, \
                 	 IPv6 VARCHAR(128))")
+            cur.execute("CREATE TABLE Readings(Id INT PRIMARY KEY AUTO_INCREMENT, \
+                     Date DATETIME, \
+                     device_id INT, \
+                     FOREIGN KEY(device_id) REFERENCES Devices(Id))")
+
     except mdb.Error, e:
   
         print "Error %d: %s" % (e.args[0],e.args[1])
@@ -48,6 +56,7 @@ def add_devices_to_db(my_list):
 
 def body(buf):
     response = json.loads(buf)
+    global devices
     devices = response["devices"]
     if devices:
         for iterator in devices:
@@ -61,10 +70,27 @@ def get_devices():
     c.setopt(pycurl.WRITEFUNCTION, body)
     c.perform()
 
+def body_device_response(buf):
+    response = json.loads(buf)
+    print response["value"]
+
+def get_device_response(device):
+    print "Getting response for device %s"% device
+    storage = StringIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, 'http://./sensors/battery?device=%s'% device)
+    c.setopt(c.PROXY, 'http://localhost:1880')
+    c.setopt(pycurl.WRITEFUNCTION, storage.write)
+    c.perform()
+    body_device_response(storage.getvalue())
+
 def main():
     print "starting"
     create_devices_table()
-    get_devices() 
+    get_devices()
+    if devices:
+        for device in devices:
+            get_device_response(device.encode("ascii"))
 
 if __name__ == "__main__":
     main()
